bb480b9fefc91072a3133ced6a5328235eeb1cc1
Alex Jonker <alex.jonker2008@gmail.com>
2025-02-02 17:55:03 +0100

    post-install fix

--- post-install.sh
+++ post-install.sh
@@ -8,6 +8,9 @@ then
     swww img ~/Pictures/Wallpapers/4pqrn9xjdcsc1.jpeg
     bash ~/Scripts/pywal.sh
 
+    betterdiscordctl install
+    spicetify auto
+
     read -p "Done! do you want to reboot? (y/n): " choice2
     if [[ "$choice2" == "y" || "$choice2" == "Y" ]]
     then
