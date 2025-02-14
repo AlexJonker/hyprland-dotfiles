0ca3740876af4cc7b7cb47a46ea9dea899df974b
Alex <81556756+MeneerAlex@users.noreply.github.com>
2025-02-14 09:21:43 +0100

    Update pywal.sh for sddm

--- scripts/pywal.sh
+++ scripts/pywal.sh
@@ -10,9 +10,45 @@ echo "----------------------------------"
 cp $wallpaper ~/.config/hypr/current_wallpaper
 cp $wallpaper /usr/share/sddm/themes/corners/backgrounds/current_wallpaper
 
+
+
 wal -i "$wallpaper" &&
-#sleep 5 &&
 pywalfox update
 pywal-discord
+killall swaync && swaync &
+
+
+
+SDDM_THEME="/usr/share/sddm/themes/corners/theme.conf"
+
+
+colors=($(cat ~/.cache/wal/colors))
+
+
+declare -A color_map=(
+    ["UserBorderColor"]="${colors[4]}"
+    ["UserColor"]="${colors[5]}"
+    ["InputColor"]="${colors[0]}"
+    ["InputTextColor"]="${colors[15]}"
+    ["InputBorderColor"]="${colors[6]}"
+    ["LoginButtonTextColor"]="${colors[7]}"
+    ["LoginButtonColor"]="${colors[12]}"
+    ["PopupColor"]="${colors[9]}"
+    ["PopupActiveColor"]="${colors[10]}"
+    ["PopupActiveTextColor"]="${colors[11]}"
+    ["SessionButtonColor"]="${colors[14]}"
+    ["SessionIconColor"]="${colors[15]}"
+    ["PowerButtonColor"]="${colors[14]}"
+    ["PowerIconColor"]="${colors[15]}"
+    ["DateColor"]="${colors[3]}"
+    ["TimeColor"]="${colors[3]}"
+)
+
+for key in "${!color_map[@]}"; do
+    sed -i "s/^$key=\"#[0-9a-fA-F]\{6\}\"/$key=\"${color_map[$key]}\"/" "$SDDM_THEME"
+done
+
+
+
 pywal-spicetify "$spicetify_theme"
-killall swaync && swaync
+
