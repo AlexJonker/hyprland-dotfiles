1d2c8a401b652083fecd95d9823096aeec855ba7
Alex Jonker <alex.jonker2008@gmail.com>
2026-04-28 09:47:46 +0200

    Update config

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd ..
+rm -fr yay
+yay -S niri xwayland-sattelite noctalia-shell kitty zen-browser nemo ttf-cascadia-code-nerd sddm brightnessctl imagemagick python power-profiles-daemon bluez gamemode cliphist wlsunset xdg-desktop-portal evolution-data-server kwallet --noconfirm --needed
+sudo systemctl enable sddm
+sudo systemctl enable bluetooth
+sudo systemctl enable power-profiles-daemon
