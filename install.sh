48e6cf088b7d0ca2e65f7b1ce5f74f1cd8bec143
Alex Jonker <alex.jonker2008@gmail.com>
2026-04-28 10:05:55 +0200

    Update

--- install.sh
+++ install.sh
@@ -12,11 +12,16 @@ printf '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n' | sudo te
 
 sudo pacman -Syu --noconfirm
 
+# Install yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 cd ..
 rm -fr yay
+
+# Install packages
 yay -S niri xwayland-sattelite noctalia-shell kitty zen-browser nemo ttf-cascadia-code-nerd sddm brightnessctl imagemagick python power-profiles-daemon bluez gamemode cliphist wlsunset xdg-desktop-portal evolution-data-server kwallet --noconfirm --needed
+
+# Enable services
 sudo systemctl enable sddm
 sudo systemctl enable power-profiles-daemon
