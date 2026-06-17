a82a5d947e3be77d11da745ee345994f4c31d26f
Alex Jonker <alex.jonker2008@gmail.com>
2026-06-17 22:20:45 +0200

    Update files

--- update-files.sh
+++ update-files.sh
@@ -1,3 +1,19 @@
-rm -fr ./.config
-mkdir ./.config
-cp -r ~/.config/{ghostty,mango,vibepanel,zlaunch} ./.config/
+rm -rf ./files
+
+ITEMS=(
+  "$HOME/.config/ghostty"
+  "$HOME/.config/mango"
+  "$HOME/.config/vibepanel"
+  "$HOME/.config/zlaunch"
+  "$HOME/.config/opencode/tui.json"
+  "$HOME/.themes/Gruvbox-B-MB-Dark-Soft-Square"
+  "$HOME/.icons/Gruvbox-Plus-Dark"
+)
+
+for src in "${ITEMS[@]}"; do
+  dest="./files${src/$HOME/}"
+
+  mkdir -p "$(dirname "$dest")"
+
+  cp -r "$src" "$dest"
+done
